b5a8287321f084170e8567c0203edf6e2b5cf6cc
quangphangiaduy <quangpro.cf4@gmail.com>
2021-01-06 03:06:03 +0700

    first commit

---
+++ temple.js
@@ -0,0 +1,28 @@
+function menuIconAnimate(x) {
+    x.classList.toggle("change");
+}
+
+// window.onscroll = function() { myFunction() };
+
+// var sliderAdvertisement = document.getElementById("slider_advertisement");
+// var stopSlider = document.getElementById('stop_slider');
+// var startSlider = document.getElementById('start_slider')
+
+// var addsticky = startSlider.offsetTop;
+// console.log(addsticky);
+
+// var removeSticky = stopSlider.offsetTop;
+// console.log(removeSticky);
+
+// function myFunction() {
+//     if (window.pageYOffset <= addsticky || window.pageYOffset > 1500) {
+//         sliderAdvertisement.classList.remove("sticky");
+//     } else {
+//         sliderAdvertisement.classList.add("sticky")
+//     }
+// }
+
+// var stopSlider = document.getElementById('stop_slider');
+// // get scroll position in px
+// console.log(stopSlider.scrollTop, stopSlider.scrollTop);
+// //&& window.pageYOffset >= removeSticky
